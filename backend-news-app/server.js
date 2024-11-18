da0d0271ffea51004f93f38122f57fc67a17348b
chiedozieu <chiedozieugwor@gmail.com>
2024-11-18 16:20:40 +0100

    database connection

--- backend-news-app/server.js
+++ backend-news-app/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+connectDB(); 
 app.get('/', (req, res) => {
     res.send('Server is running...');
 })
